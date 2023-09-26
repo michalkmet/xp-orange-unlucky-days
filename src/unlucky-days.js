518c37bdda36f1ee13879a5b4cc67d249792f376
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-09-26 09:24:49 +0200

    UAT1.3 Done

--- src/unlucky-days.js
+++ src/unlucky-days.js
@@ -1,31 +1,17 @@
 function unluckyDays(year) {
-  const firstDayStr = year + '-' + '01' + '-' + '01';
-  console.log('firstDayStr: ', firstDayStr);
-  const firstDay = new Date(firstDayStr);
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  const day = dayNames[firstDay.getDay()];
-  let isLeap = false;
-
-  console.log('day: ', day);
-
-  // isLeap = isLeap(year);
-
-  if (year === 1986) {
-    return 1;
+  let countFridays13th = 0;
+  for (let i = 1; i < 13; i++) {
+    let monthStr = i < 10 ? '0' + i : i;
+    let dayStr = year + '-' + monthStr + '-' + '13';
+    const dayFromDayStr = new Date(dayStr);
+    const day = dayNames[dayFromDayStr.getDay()];
+    if (day === 'Friday') {
+      countFridays13th++;
+    }
   }
-  return 3;
+
+  return countFridays13th;
 }
 
-function isLeap(year) {
-  console.log('isLeap');
-  // Detect the leap year
-  //First, Check the given input year is a multiple of 400. If true give output “Input year is a Leap year”.
-  //If false, Check for the secondary conditions that the input year must be a multiple of 4 but not 100.
-
-  if (year % 400 === 0) {
-    return true;
-  }
-  return false;
-}
-
-module.exports = { unluckyDays, isLeap };
+module.exports = { unluckyDays };
